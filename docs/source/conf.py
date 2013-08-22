d00bdc2146a881251e162e875db9954dab2e0725
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2013-08-22 16:59:13 +0000

    doc fixed for read the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,10 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys
+import os
+
+sys.path.insert(0, "../../")
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
